b5ce23f20fb7a029e07b9ecbac098c3ea77814c8
S <105033044+hsjsstn@users.noreply.github.com>
2024-12-25 01:27:30 +0900

    Activity 수정

--- app/src/main/java/ddwu/com/mobile/project/todaysoutfit/DiaryCalendarActivity.java
+++
@@ -1,6 +0,0 @@
-package ddwu.com.mobile.project.todaysoutfit;
-
-import android.app.Activity;
-
-public class DiaryCalendarActivity extends Activity {
-}
---
+++ app/src/main/java/ddwu/com/mobile/project/todaysoutfit/DiaryDetailActivity.kt
@@ -0,0 +1,4 @@
+package ddwu.com.mobile.project.todaysoutfit
+
+class DiaryDetailActivity {
+}
